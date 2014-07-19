570bc2d75b84c9321e0cd40a3a0d6143e8a6292d
Stefano Picozzi <StefanoPicozzi@gmail.com>
2014-07-19 11:53:37 +1000

    x

--- R/lifecoach/dolifecoach.sh
+++ R/lifecoach/dolifecoach.sh
@@ -14,6 +14,15 @@ else
   echo "Jenkins Application is already started"
 fi
 
+echo "Check jenkins client`application status ..."
+restart=`rhc app show --state nudgebldr Dopamine@1 | egrep "idle|stopped" | wc -l`
+if [ $restart != "0" ]; then
+  echo "Jenkins Application is starting"
+  rhc app start nudgebldr -p Dopamine@1
+else
+  echo "Jenkins Application is already started"
+fi
+
 echo "Check nudge database application status ..."
 restart=`rhc app show --state nudgedb -p Dopamine@1 | egrep "idle|stopped" | wc -l`
 if [ $restart != "0" ]; then
@@ -24,7 +33,7 @@ else
 fi
 
 echo "Check nudge client application status ..."
-restart=`rhc app show --state nudgedb -p Dopamine@1 | egrep "idle|stopped" | wc -l`
+restart=`rhc app show --state nudge -p Dopamine@1 | egrep "idle|stopped" | wc -l`
 if [ $restart != "0" ]; then
   echo "Nudge Client Application is starting"
   rhc app start nudge -p Dopamine@1
@@ -33,7 +42,7 @@ else
 fi
 
 echo "Check nudge server application status ..."
-restart=`rhc app show --state nudgedb -p Dopamine@1 | egrep "idle|stopped" | wc -l`
+restart=`rhc app show --state nudgeserver -p Dopamine@1 | egrep "idle|stopped" | wc -l`
 if [ $restart != "0" ]; then
   echo "Application is starting"
   rhc app start nudgeserver -p Dopamine@1
