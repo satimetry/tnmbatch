db0a337c9bcb59aeaae7826e15bb44e0a4788102
Stefano Picozzi <StefanoPicozzi@gmail.com>
2015-04-10 14:00:05 +1000

    dolifecoach crontab batch script tnmbatch

--- R/lifecoach/lifecoach-cron.sh
+++ R/lifecoach/lifecoach-cron.sh
@@ -1,2 +1,2 @@
-05 08 * * * stefanopicozzi /Users/stefanopicozzi/tnm/tnmbatch/R/lifecoach/dolifecoach.sh Dopamine@1 > /Users/stefanopicozzi/Dropbox/TheNudgeMachine/Batch/lifecoach/lifecoach-morning.log 2>&1
+05 08 * * * stefanopicozzi /Users/stefanopicozzi/tnm/tnmbatch/R/lifecoach/dolifecoach.sh Dopamine@1 > /Users/stefanopicozzi/Dropbox/TheNudgeMachine/Batch/lifecoach-morning.log 2>&1
 00 15 * * * stefanopicozzi /Users/stefanopicozzi/tnm/tnmbatch/R/lifecoach/dolifecoach.sh Dopamine@1 > /Users/stefanopicozzi/Dropbox/TheNudgeMachine/Batch/lifecoach-afternoon.log 2>&1
--- R/lifecoach/test.sh
+++ R/lifecoach/test.sh
@@ -3,12 +3,4 @@
 PASSWORD=$1
 echo $PASSWORD
 
-echo "Check jenkins server application status ..."
-restart=`rhc app show --state jenkins -p $PASSWORD | egrep "idle|stopped" | wc -l`
-echo $restart
-if [ $restart != "0" ]; then
-  echo "Jenkins Application is starting"
-else
-  echo "Jenkins Application is already started"
-fi
 
