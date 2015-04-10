2d2a1f7048c3e35f6a913ef1417283d3a70a6e37
StefanoPicozzi <StefanoPicozzi@gmail.com>
2015-04-11 08:53:57 +1000

    dolifecoach crontab batch script tnmbatch

--- R/lifecoach/test.sh
+++ R/lifecoach/test.sh
@@ -3,4 +3,15 @@
 PASSWORD=$1
 echo $PASSWORD
 
+cd /Users/stefanopicozzi/tnm/tnmbatch
+git pull
+git add .
+git commit -am "dolifecoach crontab batch script tnmbatch"
+git push
+
+cd /Users/stefanopicozzi/websites/nudge
+git pull
+git add images
+git commit -am "dolifecoach crontab batch script website images"
+git push
 
