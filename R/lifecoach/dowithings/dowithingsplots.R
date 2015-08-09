9cdac7f5003c569e7d83b7284a1630f27878e9b9
StefanoPicozzi <StefanoPicozzi@gmail.com>
2015-08-10 07:55:49 +1000

    x

--- R/lifecoach/dowithings/dowithingsplots.R
+++ R/lifecoach/dowithings/dowithingsplots.R
@@ -8,7 +8,7 @@ if ( !exists("obsname") ) { obsname <- "weight" }
 
 rooturl <- "https://nudgeserver-spicozzi.rhcloud.com/tnm/rest"
 rootdir <- "~/GitHub/tnmbatch/R/lifecoach/dowithings"
-imagesdir <- "~/Dev/websites/nudge/images"
+imagesdir <- "~/websites/nudge/images"
 ppi <- 300
 
 setwd("~/GitHub/tnmbatch/R/lifecoach")
